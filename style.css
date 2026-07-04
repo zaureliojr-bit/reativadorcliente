1d5abc311bef35467227a169360aa885ac85ea62
zaureliojr-bit <zaureliojr@gmail.com>
2026-07-04 01:51:42 -0300

    Add files via upload

---
+++ style.css
@@ -0,0 +1,15 @@
+html::before {
+  content: 'CodePen ♥ The Web';
+  position: fixed;
+  inset: 0;
+  z-index: -1;
+  margin: auto;
+  width: fit-content;
+  height: fit-content;
+  font-family: monospace;
+  opacity: 0.6;
+  background: rebeccapurple;
+  color: white;
+  padding: 4px 8px;
+  border-radius: 4px;
+}
